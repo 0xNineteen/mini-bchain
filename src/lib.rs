e3bbbdfcd9c10290fdaa33b4b0dfada43af7e1c5
0xNineteen <0x39015319@gmail.com>
2023-03-10 20:04:06 -0500

    add p2p + fork choice code

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,10 @@ use ed25519_dalek::{Keypair, Signature, PublicKey};
 use ed25519_dalek::{Signer, Verifier};
 use rand::rngs::OsRng;
 
-const HASH_BYTE_SIZE: usize = 32;
-type Sha256Bytes = [u8; HASH_BYTE_SIZE];
-type Key256Bytes = [u8; 32]; // public/private key
-type SignatureBytes = [u8; Signature::BYTE_SIZE];
+pub const HASH_BYTE_SIZE: usize = 32;
+pub type Sha256Bytes = [u8; HASH_BYTE_SIZE];
+pub type Key256Bytes = [u8; 32]; // public/private key
+pub type SignatureBytes = [u8; Signature::BYTE_SIZE];
 
 #[derive(BorshDeserialize, BorshSerialize, Default, Debug, PartialEq, Eq, Clone)]
 pub struct Account { 
@@ -18,19 +18,19 @@ pub struct Account {
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Debug, PartialEq, Eq, Clone)]
-struct Transaction { 
+pub struct Transaction { 
     pub address: Key256Bytes, 
     pub amount: u128, 
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Debug, PartialEq, Eq, Clone)]
-struct SignedTransaction { 
+pub struct SignedTransaction { 
     pub transaction: Transaction,
     pub signature: Option<SignatureBytes>
 }
 
 impl Transaction { 
-    fn digest(&self) -> Sha256Bytes { 
+    pub fn digest(&self) -> Sha256Bytes { 
         let bytes = self.try_to_vec().unwrap();
         let mut hasher = Sha256::new();
         hasher.update(bytes);
@@ -38,7 +38,7 @@ impl Transaction {
     }
 
     // consumes 
-    fn sign(self, keypair: &Keypair) -> SignedTransaction { 
+    pub fn sign(self, keypair: &Keypair) -> SignedTransaction { 
         let digest = self.digest();
         let sig = keypair.sign(digest.as_slice());
         let sig_bytes = sig.to_bytes();
@@ -47,7 +47,7 @@ impl Transaction {
 }
 
 impl SignedTransaction { 
-    fn verify(&self) -> bool { 
+    pub fn verify(&self) -> bool { 
         let digest = self.transaction.digest();
         // todo: remove these unwrap()s and return a result<>
         let publickey = PublicKey::from_bytes(self.transaction.address.as_slice()).unwrap();
@@ -57,7 +57,7 @@ impl SignedTransaction {
 }
 
 impl Account { 
-    fn digest(&self) -> Sha256Bytes { 
+    pub fn digest(&self) -> Sha256Bytes { 
         let bytes = self.try_to_vec().unwrap();
         let mut hasher = Sha256::new();
         hasher.update(bytes);
@@ -65,36 +65,49 @@ impl Account {
     }
 }
 
-#[derive(BorshDeserialize, BorshSerialize)]
-struct Block { 
-    state_root: Sha256Bytes, 
-    parent_hash: Sha256Bytes,
-    block_hash: Option<Sha256Bytes>,
+#[derive(BorshDeserialize, BorshSerialize, Debug, Default)]
+pub struct BlockHeader { 
+    pub parent_hash: Sha256Bytes,
+    pub state_root: Sha256Bytes, 
+    pub tx_root: Sha256Bytes, 
+    pub block_hash: Option<Sha256Bytes>,
+    pub nonce: u128
 }
 
-impl Block { 
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct Block { 
+    pub header: BlockHeader, 
+    pub txs: Transactions
+}
+
+impl BlockHeader { 
     pub fn genesis() -> Self { 
-        let mut block = Block { state_root: [0; HASH_BYTE_SIZE], parent_hash: [0; HASH_BYTE_SIZE],  block_hash: None };
+        let mut block = BlockHeader { state_root: [0; HASH_BYTE_SIZE], tx_root: [0; HASH_BYTE_SIZE], parent_hash: [0; HASH_BYTE_SIZE],  block_hash: None, nonce: 0};
         block.commit_block_hash();
         block
     }
 
     pub fn commit_block_hash(&mut self) { 
+        self.block_hash = Some(self.compute_block_hash());
+    }
+
+    pub fn compute_block_hash(&mut self) -> Sha256Bytes { 
         let mut hasher = Sha256::new();
         hasher.update(self.state_root);
         hasher.update(self.parent_hash);
+        hasher.update(self.nonce.to_le_bytes());
 
         let bytes: Sha256Bytes = hasher.finalize().as_slice().try_into().unwrap();
-        self.block_hash = Some(bytes);
+        bytes
     }
 }
 
 // [(digest, pubkey_bytes)]
 #[derive(BorshDeserialize, BorshSerialize)]
-struct AccountDigests(Vec<(Sha256Bytes, Key256Bytes)>);
+pub struct AccountDigests(Vec<(Sha256Bytes, Key256Bytes)>);
 
 impl AccountDigests { 
-    pub fn compute_root_digest(&self) -> Sha256Bytes { 
+    pub fn digest(&self) -> Sha256Bytes { 
         let mut hasher = Sha256::new();
         self.0
             .iter()
@@ -103,9 +116,20 @@ impl AccountDigests {
     }
 }
 
-pub fn main() { 
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct Transactions(Vec<Transaction>);
 
+impl Transactions { 
+    pub fn digest(&self) -> Sha256Bytes { 
+        let mut hasher = Sha256::new();
+        self.0
+            .iter()
+            .for_each(| d | sha2::Digest::update(&mut hasher, d.try_to_vec().unwrap().as_slice()));
+        hasher.finalize().as_slice().try_into().unwrap()
+    }
 }
+
+pub fn main() { }
 
 #[cfg(test)]
 mod tests {
@@ -114,6 +138,62 @@ mod tests {
     use super::*;
     use ed25519_dalek::{Signer, Verifier};
     use sha2::digest::Update;
+
+    #[test]
+    fn fork_choice() { 
+        let mut block_heights = HashMap::new(); 
+        let mut heads = vec![];
+        let mut head_height = 0;
+
+        let block = BlockHeader::genesis(); 
+        block_heights.insert(block.block_hash.unwrap(), 0);
+        heads.push(block.block_hash.unwrap());
+
+        // new block 
+        { 
+            let mut block_a = BlockHeader::default(); 
+            block_a.parent_hash = block.block_hash.unwrap();
+            block_a.commit_block_hash();
+
+            let parent_height = *block_heights.get(&block_a.parent_hash).unwrap();
+
+            if parent_height + 1 > head_height { 
+                heads = vec![block_a.block_hash.unwrap()];
+                head_height = parent_height + 1;
+            } else if parent_height + 1 == head_height { 
+                heads.push(block.block_hash.unwrap());
+            }
+            block_heights.insert(block_a.block_hash.unwrap(), parent_height + 1);
+        }
+    }
+
+    #[test]
+    fn block_transactions() { 
+        let txs = (0..3).map(|i| { 
+            let keypair = example_keypair();
+            Transaction { 
+                address: keypair.public.to_bytes(), 
+                amount: i
+            }
+        })
+        .collect::<Vec<_>>();
+        let txs = Transactions(txs);
+        // compute root
+        let tx_digest = txs.digest();
+
+        // pow block searching
+        let mut block = BlockHeader::genesis(); 
+        block.tx_root = tx_digest;
+
+        // u8 = 8 bits = 256
+        while block.compute_block_hash()[0] > 0 { 
+            block.nonce += 1;
+        }
+        block.commit_block_hash();
+
+        println!("{:?}", block.block_hash);
+
+    }
 
     #[test]
     fn block_state() { 
@@ -127,7 +207,7 @@ mod tests {
 
         // --- 
         // init chain
-        let genesis = Block::genesis(); 
+        let genesis = BlockHeader::genesis(); 
         db.insert(genesis.block_hash.unwrap(), genesis.try_to_vec().unwrap());
 
         let parent_block = genesis; // todo: fork choice struct
@@ -147,11 +227,11 @@ mod tests {
         let account_digests = AccountDigests(account_digests);
 
         // add state to db
-        let state_root = account_digests.compute_root_digest();
+        let state_root = account_digests.digest();
         db.insert(state_root, account_digests.try_to_vec().unwrap());
 
         // compute new block 
-        let mut block = Block { state_root, parent_hash: parent_block.block_hash.unwrap(), block_hash: None }; 
+        let mut block = BlockHeader { state_root, parent_hash: parent_block.block_hash.unwrap(), tx_root: [0; 32], block_hash: None, nonce: 0 }; 
         block.commit_block_hash();
 
         // store block in db
